eadaefa339b293eeb1d270ffe5cb22cf23485f5c
MQliferecord <118261686+MQliferecord@users.noreply.github.com>
2023-02-01 02:47:52 +0800

    Update useLoadMore.ts

--- src/hooks/useLoadMore.ts
+++ src/hooks/useLoadMore.ts
@@ -1,6 +1,14 @@
 import { debounce } from "@/utils/utils";
 import {Ref,onMounted,SetupContext} from 'vue';
 
+/**
+* @scrollTop 表示已经向上滑动隐藏的距离
+* @clientHeight 表示当前视图的距离
+* @scrollHeight 表示每页15条数据加载出来后总的高度
+* 一旦srollTop+clientHeight>=scrollHeight，就说明已经拉到了底部
+* 需要再次发送分页请求，page++，获得下一页数据
+* 这里将请求emit出去
+*/
 let useLoadMore = function(element:Ref<null|HTMLElement>,ctx:SetupContext<Record<string,any>>){
     function loadMore(){
         let containerHeight = element.value?.clientHeight;
